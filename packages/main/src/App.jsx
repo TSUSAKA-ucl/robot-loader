20318473141d824dcc3549c523dc9a05dd1ec0e9
Yuji Tsusaka <tsusaka@ucl.nuee.nagoya-u.ac.jp>
2025-09-10 17:25:48 +0900

    jointのテスト開始。revolute jointはok。start/stop stepボタン追加 resetはNG

--- packages/main/src/App.jsx
+++ packages/main/src/App.jsx
@@ -6,10 +6,12 @@ import './App.css'
 function cuboidAttrs(objDef) {
   const attrs = {};
   if (objDef.size) {
+    console.log("Cuboid ", objDef.id, " size:", objDef.size);
     attrs.width = objDef.size.x;
     attrs.height = objDef.size.y;
     attrs.depth = objDef.size.z;
   } else {
+    console.warn("Cuboid size not defined, using 1x1x1. Obj:", objDef);
     attrs.width = 1;
     attrs.height = 1;
     attrs.depth = 1;
@@ -42,17 +44,12 @@ function defineObject(objDef) {
 }
 
 function App() {
-  // const [count, setCount] = useState(0)
   const frameCounter = useRef(0);
   // ****************
   // Rapier worker
   const workerRef = useRef(null);
   const objectsRef = useRef({});
   const destinationsRef = useRef({});
-  const clickableRef = useRef(null);
-  const boxRef = useRef(null);
-  const boxRef2 = useRef(null);
-  const boxPose = useRef(null);
   useEffect(() => {
     workerRef.current = new Worker('/rapier-worker.mjs', {type: 'module'});
     const worker = workerRef.current;
@@ -66,15 +63,13 @@ function App() {
 	}
         break;
       case 'poses':
-        if (e.data.body) {
-          destinationsRef.current[e.data.body] = e.data.pose;
+        if (e.data.id && e.data.pose) {
+          destinationsRef.current[e.data.id] = e.data.pose;
           // console.log("Positions:", e.data.pose[0], e.data.pose[1]);
         }
         break;
       }
     };
-    // worker.postMessage({ type: "init", bodies: [] });
-    // setInterval(() => worker.postMessage({ type: "step" }), 16);
     return () => {
       worker.terminate();
       workerRef.current = null;
@@ -85,9 +80,8 @@ function App() {
       });
     }
   }, []);
-
   // ****************
-  // Animation loop
+  // Animation loop with Rapier updates
   const reqIdRef = useRef();
   const loop = (timestamp)=>{
     reqIdRef.current = window.requestAnimationFrame(loop) 
@@ -110,49 +104,84 @@ function App() {
       // workerRef.current?.terminate()
     }
   },[]);
-  // // ****************
-  // // box generation in a-frame
-  // useEffect(() => {
-  //   const sceneEl = document.querySelector('a-scene');
-  //   if (!sceneEl) return;
-  //   const box2 = document.createElement('a-box');
-  //   const box2attrs = {width:0.5, height:0.5, depth:0.5, color:'tomato',
-  //                      position:'1 4 -3', class:'clickable'};
-  //   Object.entries(box2attrs).forEach(([k,v])=>box2.setAttribute(k,v));
-  //   sceneEl.appendChild(box2);
-  //   boxRef2.current = box2;
-  //   return () => {
-  //     sceneEl.removeChild(box2);
-  //     boxRef2.current = null;
-  //   }
-  // },[]);
-  // ****************
-  // click! clickable object
-  useEffect(()=>{
-    const clickEl = clickableRef.current;
-    if (!clickEl) return;
-    const handleClick = (evt) => {
-      clickEl.setAttribute('color', '#'+(Math.random()*0xFFFFFF<<0).toString(16).padStart(6,'0'));
-      console.log('Box clicked!', evt);
-    };
-    clickEl.addEventListener('click',handleClick);
 
-    // clickEl.object3D.position.set(-1,2.0,-3);
-    // clickEl.object3D.quaternion.copy(new THREE.Quaternion(0,0,0.130526,0.991445));
+  // ****************
+  // Clickable Object
+  const startStopButton = useRef(null);
+  const stepButton = useRef(null);
+  const snapshotButton = useRef(false);
+  const resetButton = useRef(null);
+  const startStopRef = useRef(false);
+  useEffect(()=>{
+    const startStopEl = startStopButton.current;
+    if (!startStopEl) return;
+    const handleClick = (evt) => {
+      startStopEl.setAttribute('color', '#'+(Math.random()*0xFFFFFF<<0).toString(16).padStart(6,'0'));
+      console.log('start/stop clicked!', evt);
+      if (!startStopRef.current) {
+        workerRef.current?.postMessage({type: 'start'});
+        startStopRef.current = true;
+      } else {
+        workerRef.current?.postMessage({type: 'stop'});
+	startStopRef.current = false;
+      }
+    };
+    startStopEl.addEventListener('click',handleClick);
+
+    // const buttonDef = {'step': stepButton, 'reset': resetButton};
+    // Object.entries(buttonDef).forEach(([type,ref])=>{
+    //   const btnEl = ref.current;
+    //   if (btnEl) {
+    //     btnEl.setAttribute('class','clickable');
+    //     btnEl.setAttribute('cursor','pointer');
+    //     // btnEl.setAttribute('raycaster','objects: .clickable');
+    //     const handleBtnClick = (evt) => {
+    //       btnEl.setAttribute('color', '#'+(Math.random()*0xFFFFFF<<0).toString(16).padStart(6,'0'));
+    //       if (type !== 'step') console.log(`${type} button clicked!`, evt);
+    //       workerRef.current?.postMessage({type: type});
+    // 	};
+    //     btnEl.addEventListener('click',handleBtnClick);
+    //   }
+    // });
+        
+    const stepEl = stepButton.current;
+    if (stepEl) {
+      const handleStepClick = (evt) => {
+    	stepEl.setAttribute('color', '#'+(Math.random()*0xFFFFFF<<0).toString(16).padStart(6,'0'));
+    	// console.log('Step button clicked!', evt);
+    	workerRef.current?.postMessage({type: 'step'});
+      };
+      stepEl.addEventListener('click',handleStepClick);
+    }
+
+    const resetEl = resetButton.current;
+    if (resetEl) {
+      const handleResetClick = (evt) => {
+        resetEl.setAttribute('color', '#'+(Math.random()*0xFFFFFF<<0).toString(16).padStart(6,'0'));
+        console.log('Reset button clicked!', evt);
+        workerRef.current?.postMessage({type: 'reset'});
+      }
+      resetEl.addEventListener('click',handleResetClick);
+    }
+
     return () => {
-      clickEl.removeEventListener('click', handleClick);
+      startStopEl.removeEventListener('click', handleClick);
     };
   },[]);
 
   return (
     <>
       <a-scene>
-        <a-entity camera position="0 1.6 0" look-controls="enabled: false"></a-entity>
-        {/* ここが重要: シーン直下に cursor */}
+        <a-entity camera position="0 1.6 2.0" look-controls="enabled: false"></a-entity>
+        {/* clickableをvr以外でclickするには、ここが重要: シーン直下に cursor */}
         <a-entity cursor="rayOrigin: mouse"
                   raycaster="objects: .clickable"></a-entity>
-        <a-sphere ref={clickableRef} class="clickable"
+        <a-sphere ref={startStopButton} class="clickable"
           position="0 1.25 -10" radius="1.25" color="#EF2D5E"></a-sphere>
+        <a-sphere ref={stepButton} class="clickable"
+          position="0 3.25 -10" radius="0.75" color="#4CC3D9"></a-sphere>
+	<a-sphere ref={resetButton} class="clickable"
+          position="0 4.50 -10" radius="0.5" color="#7BC8A4"></a-sphere>
         <a-cylinder position="1 0.75 -3"
                     radius="0.5" height="1.5" color="#FFC65D"></a-cylinder>
         <a-sky color="#ECECEC"></a-sky>
@@ -162,13 +191,3 @@ function App() {
 }
 
 export default App
-
-// // 元の色
-// const color = new THREE.Color("#7BC8A4");
-// // HSL取得
-// const hsl = {};
-// color.getHSL(hsl);
-// // 明度だけ半分に
-// color.setHSL(hsl.h, hsl.s, hsl.l * 0.5);
-// // A-Frame要素に反映（hexに戻して渡す）
-// box.setAttribute("material", { color: `#${color.getHexString()}` });
--- packages/rapier-worker/src/rapier-worker.js
+++ packages/rapier-worker/src/rapier-worker.js
@@ -1,24 +1,8 @@
 import RAPIER from '@dimforge/rapier3d-compat'
 
-function writePoseToMessage(body, message) {
-  if (!body) return;
-  const position = body.translation();
-  const orientation = body.rotation();
-  message.pose = [position.x, position.y, position.z,
-		  orientation.w, orientation.x, orientation.y, orientation.z
-		 ];
-}
-function writeCuboidSizeToMessage(collider, message) {
-  if (!collider) return;
-  const shape = collider.shape;
-  if (shape && shape.type === 'Cuboid') {
-    message.size = {x: shape.halfExtents.x * 2,
-		    y: shape.halfExtents.y * 2,
-		    z: shape.halfExtents.z * 2
-		   };
-  }
-}
+const sharedBodies = {};
 
+run_simulation();
 async function run_simulation() {
   await RAPIER.init();
   // Use the RAPIER module here.
@@ -35,53 +19,164 @@ async function run_simulation() {
   self.postMessage(groundPlaneMsg);
   
   // Create a dynamic rigid-body.
-  let rigidBodyDesc = RAPIER.RigidBodyDesc.dynamic()
-      .setTranslation(-1.0, 2.0, -3.0)
-      .setRotation({w: 0.991445, x:0.0, y:0.0, z:0.130526});
-  let rigidBody = world.createRigidBody(rigidBodyDesc);
-  // Create a cuboid collider attached to the dynamic rigidBody.
-  let colliderDesc = RAPIER.ColliderDesc.cuboid(0.5, 0.6, 0.2);
-  let collider = world.createCollider(colliderDesc, rigidBody);
-  const box1msg = {type: 'definition', id: 'box1', shape: 'cuboid',
-		   color: "#4CC3D9" };
-  writeCuboidSizeToMessage(collider, box1msg);
-  writePoseToMessage(rigidBody, box1msg);
+  const box1id = 'box1';
+  const {box: box1, boxCollider: box1Collider, boxmsg: box1msg}
+  	= createBox(world,
+		    {x: -1.0, y: 2.0, z: -3.0},	// position
+		    {w: 0.991445, x:0.0, y:0.0, z:0.130526}, // orientation
+		    {x: 0.4, y: 0.6, z: 0.2}, // size
+		    "#4CC3D9", // light blue color
+		    box1id);
   self.postMessage(box1msg);
+  sharedBodies[box1id] = box1;
   
   // Create a dynamic rigid-body.
-  let box2Desc = RAPIER.RigidBodyDesc.dynamic()
-      .setTranslation(-1.0, 4.0, -3.0)
-      .setRotation({w: 0.991445, x:0.0, y:0.0, z:-0.130526});
-  let box2 = world.createRigidBody(box2Desc);
-  // Create a cuboid collider attached to the dynamic rigidBody.
-  let box2ColliderDesc = RAPIER.ColliderDesc.cuboid(0.5, 0.6, 0.2);
-  let box2Collider = world.createCollider(box2ColliderDesc, box2);
-  const box2msg = {type: 'definition',
-		   id: 'box2',
-		   shape: 'cuboid',
-		   size: {x:0.5*2, y:0.6*2, z:0.2*2},
-		   color: "#FF6347"
-		  };
-  writePoseToMessage(box2, box2msg);
+  const box2id = 'box2';
+  const {box: box2, boxCollider: box2Collider, boxmsg: box2msg}
+	= createBox(world,
+		    {x: -1.0, y: 4.0, z: -3.0},	// position
+		    {w: 0.991445, x:0.0, y:0.0, z:-0.130526},
+		    {x: 0.25, y: 0.05, z: 0.6},	// size
+		    "#FF6347",	// tomato color
+		    box2id);
   self.postMessage(box2msg);
+  sharedBodies[box2id] = box2;
+
+  // Create a dynamic rigid-body.
+  const box3id = 'box3';
+  const {box: box3, boxCollider: box3Collider, boxmsg: box3msg}
+	= createBox(world,
+		    {x: -1.0, y: 3.5, z: -3.0}, // position
+		    {w: 0.991445, x:0.0, y:0.0, z:-0.130526},
+		    {x: 0.25, y: 0.05, z: 0.6}, // size
+		    'blue',
+		    box3id);
+  self.postMessage(box3msg);
+  sharedBodies[box3id] = box3;
+
+  // Create a prismatic joint between the two boxes.
+  const x = { x: 1.0, y: 0.0, z: 0.0 };
+  const y = { x: 0.0, y: 1.0, z: 0.0 };
+  const z = { x: 0.0, y: 0.0, z: 1.0 };
+  //
+  // let pnt = { x: 0.0, y: 0.0, z: -1.0 };
+  // let params = RAPIER.JointData.prismatic(pnt, y, y);
+  // params.limitsEnabled = true;
+  // params.limits = [-1.5, 0.5];
+  // let joint = world.createImpulseJoint(params, box1, box2, true);
+  const pnt1a = { x: 0.0, y: -0.6, z: 0.2 };
+  const pnt1b = { x: 0.0, y: 0.05, z: -0.6 };
+  let jntParams = RAPIER.JointData.revolute(pnt1a, pnt1b, x);
+  jntParams.limitsEnabled = true;
+  let joint = world.createImpulseJoint(jntParams, box1, box2, true);
+  //
+  const pnt2a = { x: 0.0, y: 0.6, z: 0.2 };
+  const pnt2b = { x: 0.0, y: 0.05, z: -0.6 };
+  let jntParams2 = RAPIER.JointData.revolute(pnt2a, pnt2b, x);
+  jntParams2.limitsEnabled = true;
+  let joint2 = world.createImpulseJoint(jntParams2, box1, box3, true);
+
 
   // Game loop. Replace by your own game loop system.
+
+  let snapshot = world.snapshot;
+  let doStep = false;
+  let singleStep = false;
+  self.onmessage = (e) => {
+    const data = e.data;
+    switch (data.type) {
+    case 'reset':
+      if (snapshot) {
+	console.log("Resetting simulation");
+	world = RAPIER.World.restoreSnapshot(snapshot);
+      }
+      break;
+    case 'stop':
+      doStep = false;
+      break;
+    case 'start':
+      if (!doStep) {
+	doStep = true;
+	singleStep = false;
+      }
+      break;
+    case 'step':
+      if (!doStep) {
+	singleStep = true;
+	doStep = true;
+      }
+      break;
+    case 'snapshot':
+      snapshot = world.snapshot;
+      console.log("Snapshotting simulation");
+      break;
+    default:
+      console.warn("Worker: Unknown message", data);
+    }
+  }
+      
+  const workerTimeStep = 1.0 / 60.0;
+  const loopTimeStep = workerTimeStep * 1000;
+  world.timestep = workerTimeStep;
   let gameLoop = () => {
     // Step the simulation forward.  
-    world.step();
-
+    if (doStep) {
+      world.step();
+      if (singleStep) { doStep = false; singleStep = false; }
+    }
+    if (!snapshot) {
+      snapshot = world.snapshot;
+    }
     // Get and print the rigid-body's position.
-    const msg = {type: 'poses', body: 'box1'};
-    writePoseToMessage(rigidBody, msg);
-    self.postMessage(msg);
-    msg.body = 'box2';
-    writePoseToMessage(box2, msg);
-    self.postMessage(msg);
-
-    setTimeout(gameLoop, 16);
+    const msg = {type: 'poses', id: null, pose: null};
+    Object.keys(sharedBodies).forEach((id) => {
+      msg.id = id;
+      writePoseToMessage(sharedBodies[id], msg);
+      self.postMessage(msg);
+    });
+    setTimeout(gameLoop, loopTimeStep);
   };
 
   gameLoop();
 }
 
-run_simulation();
+
+
+function writePoseToMessage(body, message) {
+  if (!body) return;
+  const position = body.translation();
+  const orientation = body.rotation();
+  message.pose = [position.x, position.y, position.z,
+		  orientation.w, orientation.x, orientation.y, orientation.z
+		 ];
+}
+function writeCuboidSizeToMessage(collider, message) {
+  if (!collider) return;
+  const shape = collider.shape;
+  if (shape && shape.type === RAPIER.ShapeType.Cuboid) {
+    message.size = {x: shape.halfExtents.x * 2,
+		    y: shape.halfExtents.y * 2,
+		    z: shape.halfExtents.z * 2
+		   };
+    // console.log("msg with Cuboid size:", message);
+  } else {
+    console.warn("Collider shape is not Cuboid:", shape);
+    console.warn("msg without size:", message);
+  }
+}
+
+function createBox(world, position, rotation, size, color, id) {
+  // Create a dynamic rigid-body.
+  let boxDesc = RAPIER.RigidBodyDesc.dynamic()
+      .setTranslation(position.x, position.y, position.z)
+      .setRotation(rotation);
+  let box = world.createRigidBody(boxDesc);
+  // Create a cuboid collider attached to the dynamic rigidBody.
+  let boxColliderDesc = RAPIER.ColliderDesc.cuboid(size.x, size.y, size.z);
+  let boxCollider = world.createCollider(boxColliderDesc, box);
+  const boxmsg = {type: 'definition', id: id, shape: 'cuboid',
+		   color: color };
+  writeCuboidSizeToMessage(boxCollider, boxmsg);
+  writePoseToMessage(box, boxmsg);
+  return {box, boxCollider, boxmsg};
+}
