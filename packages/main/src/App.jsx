2b24b12fd382a208784466eaea7d704efe4cb0d4
Yuji Tsusaka <tsusaka@ucl.nuee.nagoya-u.ac.jp>
2025-09-10 14:03:05 +0900

    Rapierの定義を実行時に動的に反映させる

--- packages/main/src/App.jsx
+++ packages/main/src/App.jsx
@@ -3,28 +3,87 @@ import 'aframe'
 const THREE = window.AFRAME.THREE;
 import './App.css'
 
+function cuboidAttrs(objDef) {
+  const attrs = {};
+  if (objDef.size) {
+    attrs.width = objDef.size.x;
+    attrs.height = objDef.size.y;
+    attrs.depth = objDef.size.z;
+  } else {
+    attrs.width = 1;
+    attrs.height = 1;
+    attrs.depth = 1;
+  }
+  attrs.color = objDef.color || 'gray';
+  return attrs;
+}
+
+function defineObject(objDef) {
+  const sceneEl = document.querySelector('a-scene');
+  if (!sceneEl) return null;
+  let el = null;
+  switch (objDef.shape) {
+  case 'cuboid':
+    el = document.createElement('a-box');
+    const elAttrs = cuboidAttrs(objDef);
+    Object.entries(elAttrs).forEach(([k,v])=>el.setAttribute(k,v));
+    break;
+  default:
+    console.warn("Unknown shape:", objDef.shape);
+  }
+  if (!el) return null;
+  if (objDef.pose) {
+    el.object3D.position.set(objDef.pose[0], objDef.pose[1], objDef.pose[2]);
+    el.object3D.quaternion.set(objDef.pose[4], objDef.pose[5], objDef.pose[6],
+                               objDef.pose[3]);
+  }
+  sceneEl.appendChild(el);
+  return el;
+}
+
 function App() {
   // const [count, setCount] = useState(0)
   const frameCounter = useRef(0);
   // ****************
   // Rapier worker
   const workerRef = useRef(null);
+  const objectsRef = useRef({});
+  const destinationsRef = useRef({});
+  const clickableRef = useRef(null);
   const boxRef = useRef(null);
+  const boxRef2 = useRef(null);
   const boxPose = useRef(null);
   useEffect(() => {
     workerRef.current = new Worker('/rapier-worker.mjs', {type: 'module'});
     const worker = workerRef.current;
     worker.onmessage = (e) => {
-      if (e.data.type === 'poses') {
-        if (e.data.body === 'box1') {
-          boxPose.current = e.data.pose;
+      switch (e.data.type) {
+      case 'definition':
+        const el = defineObject(e.data);
+        if (el) {
+	  objectsRef.current[e.data.id] = el;
+          destinationsRef.current[e.data.id] = e.data.pose;
+	}
+        break;
+      case 'poses':
+        if (e.data.body) {
+          destinationsRef.current[e.data.body] = e.data.pose;
           // console.log("Positions:", e.data.pose[0], e.data.pose[1]);
         }
+        break;
       }
     };
-
-    worker.postMessage({ type: "init", bodies: [] });
+    // worker.postMessage({ type: "init", bodies: [] });
     // setInterval(() => worker.postMessage({ type: "step" }), 16);
+    return () => {
+      worker.terminate();
+      workerRef.current = null;
+      Object.values(objectsRef.current).forEach((el) => {
+	if (el.parentNode) {
+	  el.parentNode.removeChild(el);
+	}
+      });
+    }
   }, []);
 
   // ****************
@@ -32,16 +91,14 @@ function App() {
   const reqIdRef = useRef();
   const loop = (timestamp)=>{
     reqIdRef.current = window.requestAnimationFrame(loop) 
-    const boxEl = boxRef.current;
-    if (boxEl) {
-      const obj = boxEl.object3D;
-      const p = boxPose.current;
+    for (const [id, el] of Object.entries(objectsRef.current)) {
+      const p = destinationsRef.current[id];
       if (p) {
-        obj.position.copy(new THREE.Vector3(p[0], p[1], p[2]));
-        obj.quaternion.copy(new THREE.Quaternion(p[4], p[5], p[6], p[3]));
-        if (frameCounter.current < 30) {
-          console.log("Box pos:", p[0], p[1], p[2]);
-	}
+	el.object3D.position.copy(new THREE.Vector3(p[0], p[1], p[2]));
+	el.object3D.quaternion.copy(new THREE.Quaternion(p[4], p[5], p[6], p[3]));
+	// if (frameCounter.current < 30) {
+	//   console.log(`Obj ${id} pos:`, el.object3D.position);
+        // }
       }
     }
     frameCounter.current += 1;
@@ -53,20 +110,37 @@ function App() {
       // workerRef.current?.terminate()
     }
   },[]);
+  // // ****************
+  // // box generation in a-frame
+  // useEffect(() => {
+  //   const sceneEl = document.querySelector('a-scene');
+  //   if (!sceneEl) return;
+  //   const box2 = document.createElement('a-box');
+  //   const box2attrs = {width:0.5, height:0.5, depth:0.5, color:'tomato',
+  //                      position:'1 4 -3', class:'clickable'};
+  //   Object.entries(box2attrs).forEach(([k,v])=>box2.setAttribute(k,v));
+  //   sceneEl.appendChild(box2);
+  //   boxRef2.current = box2;
+  //   return () => {
+  //     sceneEl.removeChild(box2);
+  //     boxRef2.current = null;
+  //   }
+  // },[]);
   // ****************
-  // box click!
+  // click! clickable object
   useEffect(()=>{
-    const boxEl = boxRef.current;
-    if (!boxEl) return;
+    const clickEl = clickableRef.current;
+    if (!clickEl) return;
     const handleClick = (evt) => {
+      clickEl.setAttribute('color', '#'+(Math.random()*0xFFFFFF<<0).toString(16).padStart(6,'0'));
       console.log('Box clicked!', evt);
     };
-    boxEl.addEventListener('click',handleClick);
+    clickEl.addEventListener('click',handleClick);
 
-    boxEl.object3D.position.set(-1,2.0,-3);
-    boxEl.object3D.quaternion.copy(new THREE.Quaternion(0,0,0.130526,0.991445));
+    // clickEl.object3D.position.set(-1,2.0,-3);
+    // clickEl.object3D.quaternion.copy(new THREE.Quaternion(0,0,0.130526,0.991445));
     return () => {
-      boxEl.removeEventListener('click', handleClick);
+      clickEl.removeEventListener('click', handleClick);
     };
   },[]);
 
@@ -77,14 +151,10 @@ function App() {
         {/* ここが重要: シーン直下に cursor */}
         <a-entity cursor="rayOrigin: mouse"
                   raycaster="objects: .clickable"></a-entity>
-        <a-box ref={boxRef} class="clickable"
-               width="1.0" height="1.2" depth="0.4"
-               color="#4CC3D9"></a-box>
-        <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere>
+        <a-sphere ref={clickableRef} class="clickable"
+          position="0 1.25 -10" radius="1.25" color="#EF2D5E"></a-sphere>
         <a-cylinder position="1 0.75 -3"
                     radius="0.5" height="1.5" color="#FFC65D"></a-cylinder>
-        <a-plane position="0 0.1 -4" rotation="-90 0 0"
-                 width="5" height="5" color="#7BC8A4"></a-plane>
         <a-sky color="#ECECEC"></a-sky>
     </a-scene>
     </>
@@ -92,3 +162,13 @@ function App() {
 }
 
 export default App
+
+// // 元の色
+// const color = new THREE.Color("#7BC8A4");
+// // HSL取得
+// const hsl = {};
+// color.getHSL(hsl);
+// // 明度だけ半分に
+// color.setHSL(hsl.h, hsl.s, hsl.l * 0.5);
+// // A-Frame要素に反映（hexに戻して渡す）
+// box.setAttribute("material", { color: `#${color.getHexString()}` });
--- packages/rapier-worker/src/rapier-worker.js
+++ packages/rapier-worker/src/rapier-worker.js
@@ -1,5 +1,24 @@
 import RAPIER from '@dimforge/rapier3d-compat'
 
+function writePoseToMessage(body, message) {
+  if (!body) return;
+  const position = body.translation();
+  const orientation = body.rotation();
+  message.pose = [position.x, position.y, position.z,
+		  orientation.w, orientation.x, orientation.y, orientation.z
+		 ];
+}
+function writeCuboidSizeToMessage(collider, message) {
+  if (!collider) return;
+  const shape = collider.shape;
+  if (shape && shape.type === 'Cuboid') {
+    message.size = {x: shape.halfExtents.x * 2,
+		    y: shape.halfExtents.y * 2,
+		    z: shape.halfExtents.z * 2
+		   };
+  }
+}
+
 async function run_simulation() {
   await RAPIER.init();
   // Use the RAPIER module here.
@@ -8,17 +27,43 @@ async function run_simulation() {
 
   // Create the ground
   let groundColliderDesc = RAPIER.ColliderDesc.cuboid(10.5, 0.1, 10.5);
-  world.createCollider(groundColliderDesc);
+  const groundCollider = world.createCollider(groundColliderDesc);
+  const groundPlaneMsg = {type: 'definition', id: 'plane1', shape: 'cuboid',
+			  color: "#7BC8A4"};
+  writeCuboidSizeToMessage(groundCollider, groundPlaneMsg);
+  writePoseToMessage(groundCollider, groundPlaneMsg);
+  self.postMessage(groundPlaneMsg);
   
   // Create a dynamic rigid-body.
   let rigidBodyDesc = RAPIER.RigidBodyDesc.dynamic()
       .setTranslation(-1.0, 2.0, -3.0)
       .setRotation({w: 0.991445, x:0.0, y:0.0, z:0.130526});
   let rigidBody = world.createRigidBody(rigidBodyDesc);
-
   // Create a cuboid collider attached to the dynamic rigidBody.
   let colliderDesc = RAPIER.ColliderDesc.cuboid(0.5, 0.6, 0.2);
   let collider = world.createCollider(colliderDesc, rigidBody);
+  const box1msg = {type: 'definition', id: 'box1', shape: 'cuboid',
+		   color: "#4CC3D9" };
+  writeCuboidSizeToMessage(collider, box1msg);
+  writePoseToMessage(rigidBody, box1msg);
+  self.postMessage(box1msg);
+  
+  // Create a dynamic rigid-body.
+  let box2Desc = RAPIER.RigidBodyDesc.dynamic()
+      .setTranslation(-1.0, 4.0, -3.0)
+      .setRotation({w: 0.991445, x:0.0, y:0.0, z:-0.130526});
+  let box2 = world.createRigidBody(box2Desc);
+  // Create a cuboid collider attached to the dynamic rigidBody.
+  let box2ColliderDesc = RAPIER.ColliderDesc.cuboid(0.5, 0.6, 0.2);
+  let box2Collider = world.createCollider(box2ColliderDesc, box2);
+  const box2msg = {type: 'definition',
+		   id: 'box2',
+		   shape: 'cuboid',
+		   size: {x:0.5*2, y:0.6*2, z:0.2*2},
+		   color: "#FF6347"
+		  };
+  writePoseToMessage(box2, box2msg);
+  self.postMessage(box2msg);
 
   // Game loop. Replace by your own game loop system.
   let gameLoop = () => {
@@ -26,13 +71,13 @@ async function run_simulation() {
     world.step();
 
     // Get and print the rigid-body's position.
-    let position = rigidBody.translation();
-    let orientation = rigidBody.rotation();
-    // console.log("Rigid-body position: ", position.x, position.y);
-    self.postMessage({type: 'poses', body: 'box1',
-		      pose: [position.x, position.y, position.z,
-			     orientation.w, orientation.x, orientation.y, orientation.z
-			    ]});
+    const msg = {type: 'poses', body: 'box1'};
+    writePoseToMessage(rigidBody, msg);
+    self.postMessage(msg);
+    msg.body = 'box2';
+    writePoseToMessage(box2, msg);
+    self.postMessage(msg);
+
     setTimeout(gameLoop, 16);
   };
 
